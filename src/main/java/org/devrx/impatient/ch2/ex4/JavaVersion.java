bd453033c9c81f352570f9973ee6fe8f11c3b938
Francis Devereux <francis@bright-interactive.co.uk>
2016-02-28 18:14:53 +0000

    Chapter 2 exercise 4

---
+++ src/main/java/org/devrx/impatient/ch2/ex4/JavaVersion.java
@@ -0,0 +1,7 @@
+package org.devrx.impatient.ch2.ex4;
+
+public class JavaVersion {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) System.out.println(i);
+    }
+}
---
+++ src/main/java/org/devrx/impatient/ch2/ex4/ScalaVersion.scala
@@ -0,0 +1,7 @@
+package org.devrx.impatient.ch2.ex4
+
+object ScalaVersion {
+  def main(args: Array[String]) {
+    for (i <- 10.to(0,-1)) println(i)
+  }
+}
