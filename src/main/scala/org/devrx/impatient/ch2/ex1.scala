6f0faf3255e0c9c0d2a39ae38bc779ef739ba0d3
Francis Devereux <francis@bright-interactive.co.uk>
2016-02-28 17:37:13 +0000

    Extend signum to accept any Numeric type

--- src/main/scala/org/devrx/impatient/ch2/ex1.scala
+++ src/main/scala/org/devrx/impatient/ch2/ex1.scala
@@ -1,9 +1,11 @@
 package org.devrx.impatient.ch2
 
 package object ex1 {
-  def signum(n: Int): Int = {
-    if (n == 0) 0
-    else if (n < 0) -1
-    else 1
+  def signum[T](n: T)(implicit numeric: scala.math.Numeric[T]): Int = {
+    import numeric._
+
+    if (gt(n, zero)) 1
+    else if (lt(n, zero)) -1
+    else 0
   }
 }
--- src/test/scala/org/devrx/impatient/ch2/ex1/SignumSpec.scala
+++ src/test/scala/org/devrx/impatient/ch2/ex1/SignumSpec.scala
@@ -14,4 +14,16 @@ class SignumSpec extends FlatSpec with Matchers {
   it should "return 0 for zero" in {
     signum(0) should be (0)
   }
+
+  it should "return 1 for positive Doubles" in {
+    signum(0.01: Double) should be (1)
+  }
+
+  it should "return -1 for negative Doubles" in {
+    signum(-0.02: Double) should be (-1)
+  }
+
+  it should "return 0 for zero as a Double" in {
+    signum(0.0: Double) should be (0)
+  }
 }
